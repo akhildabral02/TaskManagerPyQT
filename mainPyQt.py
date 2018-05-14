be614c2b7aaeb492679df40c6800d5d12c766fed
akhildabral02@gmail.com <Rocket2#>
2018-05-14 19:56:39 +0530

    Menu Bar Addedd

--- mainPyQt.py
+++ mainPyQt.py
@@ -1,14 +1,21 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
 from PyQt5.QtGui import QIcon
 
-class Application(QWidget):
+class Application(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         self.setGeometry(300,300,600,600)
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('File')
+        editMenu = mainMenu.addMenu('Edit')
+        viewMenu = mainMenu.addMenu('View')
+        searchMenu = mainMenu.addMenu('Search')
+        toolsMenu = mainMenu.addMenu('Tools')
+        helpMenu = mainMenu.addMenu('Help')
         self.setWindowTitle("Task Manager")
         self.setWindowIcon(QIcon("C:\\Users\\adabral\\Downloads\\task_manager_image.png"))
         self.show()
