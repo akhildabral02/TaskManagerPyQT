4cb6ec65af7aad9f0ac48e51ffca615cd26b4a6d
akhildabral02@gmail.com <Rocket2#>
2018-05-15 08:20:56 +0530

    Menu Bar Addedd

--- mainPyQt.py
+++ mainPyQt.py
@@ -1,14 +1,23 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow,QVBoxLayout,QGridLayout,QPushButton,QGroupBox,QCheckBox,QDialog
 from PyQt5.QtGui import QIcon
 
-class Application(QMainWindow):
+class Application(QDialog,QMainWindow):
     def __init__(self):
         super().__init__()
+        self.title = "Task Manager -PyQt5"
+        self.left = 100
+        self.top = 200
+        self.width = 600
+        self.height = 300
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300,300,600,600)
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowIcon(QIcon("C:\\Users\\adabral\\Downloads\\task_manager_image.png"))
+
+        # Lets start with Basic Menu Bar Creation
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('File')
         editMenu = mainMenu.addMenu('Edit')
@@ -16,10 +25,32 @@ class Application(QMainWindow):
         searchMenu = mainMenu.addMenu('Search')
         toolsMenu = mainMenu.addMenu('Tools')
         helpMenu = mainMenu.addMenu('Help')
-        self.setWindowTitle("Task Manager")
-        self.setWindowIcon(QIcon("C:\\Users\\adabral\\Downloads\\task_manager_image.png"))
+
+        self.createGridLayout()
+
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox)
+        self.setLayout(windowLayout)
+
         self.show()
 
+    def createGridLayout(self):
+        self.horizontalGroupBox = QGroupBox("Grid")
+        layout = QGridLayout()
+        layout.setColumnStretch(1, 4)
+        layout.setColumnStretch(2, 4)
+
+        layout.addWidget(QCheckBox('1'), 0, 0)
+        layout.addWidget(QPushButton('2'), 0, 1)
+        layout.addWidget(QPushButton('3'), 0, 2)
+        layout.addWidget(QPushButton('4'), 1, 0)
+        layout.addWidget(QPushButton('5'), 1, 1)
+        layout.addWidget(QPushButton('6'), 1, 2)
+        layout.addWidget(QPushButton('7'), 2, 0)
+        layout.addWidget(QPushButton('8'), 2, 1)
+        layout.addWidget(QPushButton('9'), 2, 2)
+
+        self.horizontalGroupBox.setLayout(layout)
 
 
 if __name__ == "__main__":
