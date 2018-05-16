898a3170a3097ea2a9a29f9a84a5a7fa9e9a654a
akhildabral02@gmail.com <akhildabral02@gmail.com>
2018-05-16 10:47:47 +0530

    This will strike through the task description as soon as we check the checkmark showing task is completed

--- mainPyQt.py
+++ mainPyQt.py
@@ -1,11 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow,QVBoxLayout,QGridLayout,QPushButton,QGroupBox,QCheckBox,QDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow,QLabel
 from PyQt5.QtGui import QIcon
 
-class Application(QMainWindow,QDialog):
+class Application(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self,parent=None)
-        QDialog.__init__(self,parent=None)
+        super().__init__()
         self.title = "Task Manager -PyQt5"
         self.left = 100
         self.top = 200
@@ -16,7 +15,7 @@ class Application(QMainWindow,QDialog):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setWindowIcon(QIcon("C:\\Users\\adabral\\Downloads\\task_manager_image.png"))
+        self.setWindowIcon(QIcon("C:\\git\\TaskManagerPyQT\\task_manager_image.png"))
 
         # Lets start with Basic Menu Bar Creation
         mainMenu = self.menuBar()
@@ -27,31 +26,11 @@ class Application(QMainWindow,QDialog):
         toolsMenu = mainMenu.addMenu('Tools')
         helpMenu = mainMenu.addMenu('Help')
 
-        self.createGridLayout()
+        # Label needed for Task Status
+        self.label = QLabel("STATUS")
 
-        windowLayout = QVBoxLayout()
-        windowLayout.addWidget(self.horizontalGroupBox)
-        self.setLayout(windowLayout)
 
         self.show()
-
-    def createGridLayout(self):
-        self.horizontalGroupBox = QGroupBox("Grid")
-        layout = QGridLayout()
-        layout.setColumnStretch(1, 4)
-        layout.setColumnStretch(2, 4)
-
-        layout.addWidget(QCheckBox('1'), 0, 0)
-        layout.addWidget(QPushButton('2'), 0, 1)
-        layout.addWidget(QPushButton('3'), 0, 2)
-        layout.addWidget(QPushButton('4'), 1, 0)
-        layout.addWidget(QPushButton('5'), 1, 1)
-        layout.addWidget(QPushButton('6'), 1, 2)
-        layout.addWidget(QPushButton('7'), 2, 0)
-        layout.addWidget(QPushButton('8'), 2, 1)
-        layout.addWidget(QPushButton('9'), 2, 2)
-
-        self.horizontalGroupBox.setLayout(layout)
 
 
 if __name__ == "__main__":
